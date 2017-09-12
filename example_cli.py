827c16064be83f4ecde00a4c5770b7398e94111f
TheMushrr00m <funji_2302@outlook.es>
2017-09-12 03:32:19 -0500

    CLI example finished

---
+++ example_cli.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tercer método: acceso cli"""
+import datetime
+import hug
+
+
+@hug.cli()
+@hug.get(examples="name=Jhon Doe&age=30")
+@hug.local()
+def say_hello(name: hug.types.text, age: hug.types.number, hug_timer=3):
+    """Decimos hola al usuario y calculamos su año de nacimiento"""
+    year_of_birth = datetime.datetime.now().year - age
+    return {
+        'message': "Hola {0}, naciste el año {1}".format(name, year_of_birth),
+        'took': float(hug_timer)
+    }
+
+
+if __name__ == '__main__':
+    print(say_hello("Gabriel", 23))
